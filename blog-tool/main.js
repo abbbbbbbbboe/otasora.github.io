f64aa7afdb9ee7aacbbd34a79306be357183df25
otasora <ootaoota0038@gmail.com>
2026-03-09 19:20:20 +0900

    blogtoolタイプ別対応

--- blog-tool/main.js
+++ blog-tool/main.js
@@ -21,54 +21,73 @@ input.addEventListener('change', function (event) {
         const header = lines[0];
         const rows = lines.slice(1);
 
+        
+
         const result = rows.map((row) => {
             const cols = row.split(",");
 
-            const tag = cols[0];
+            const type = cols[0];
+            
+            let line;
+
+            if (type === 'divider') {
+            line = `{type:"${type}"},`;
+            return line;
+            }
+
+            if (type === 'skipbutton') {
+                const text =cols[1];
+                const id =cols[2];
+                const label =cols[3];
+                const mobileLavel =cols[4];
+                line = `{ type: "${type}", id: "${id}", label: "${label}", mobile_label: "${mobileLavel}", text: "${text}" },`
+                return line;
+
+            }
+
+            if (type === 'img-button') {
+                const targetId =cols[1];
+                const label =cols[2];
+                line = `{ type: "${type}", targetId: "${targetId}", label: "${label}"},`
+                return line;
+            }
+
+
             const text = cols[1];
             const className = cols[2];
 
             const obj = {
-                type: tag,
+                type: type,
                 text: text,
             };
 
             if (className && className.trim() !== "") {
                 obj.class = className
-                .split("|")
-                .map(c => c.trim())
-                .filter(c => c !== "");
+                    .split("|")
+                    .map(c => c.trim())
+                    .filter(c => c !== "");
             }
-
+            
+            if (obj.type === 'a'){
+                const hyperlink = cols[3];
+                line = `{type:"${obj.type}", text:\`${obj.text}\`, link: "${hyperlink}",`;
+            } else{
+                line = `{type:"${obj.type}", text:\`${obj.text}\`,`;
+            }
           
-
-            let line = `{type:"${obj.type}", text:\`${obj.text}\`,`;
             if (obj.class) {
                 line += ` class: [${obj.class.map(c => `"${c}"`).join(",")}]`
             }
 
             line += "},";
-
             return line;
-            
-            
-
-
-            // if (className && className.trim() !== "") {
-            //     return `{type:"${tag}", text:\`${text}\`, class: "${className}"},`;
-            // } else {
-            //     return `{type:"${tag}", text:\`${text}\`,`;
-            // }
-
-        
         });
 
-        
+
 
         const mainElements = document.querySelector('main');
         const copyButtonElement = document.getElementById('copyButton');
 
-        // const output = result.join("\n");
         result.forEach(value => {
             const pElement = document.createElement('p');
             pElement.textContent = value;
